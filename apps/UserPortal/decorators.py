2754ddee9c65c31ff3381c8b3a0d89446f440c22
petermamaril <mamaril_3180850@letran-calamba.edu.ph>
2022-09-21 01:22:17 +0800

    init

--- apps/UserPortal/decorators.py
+++ apps/UserPortal/decorators.py
@@ -4,8 +4,9 @@ from django.shortcuts import redirect
 def unauthenticated_user(view_func):
     def wrapper_func(request, *args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('index')
+            return redirect('service_portal')
         else:
             return view_func(request, *args, **kwargs)
 
     return wrapper_func
+
