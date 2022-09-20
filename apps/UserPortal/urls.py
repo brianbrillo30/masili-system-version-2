2705b036d9a6e490c00eb37931e3e781b4b95af7
Brian Brillo <brillo_3181047@letran-calamba.edu.ph>
2022-09-20 21:35:26 +0800

    Initial Commit

--- apps/UserPortal/urls.py
+++ apps/UserPortal/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path ('Home/', index, name='index'),
     path ('About/', about, name='about'),
+    path ('ServicePortal/', ServicesPortal, name='service_portal'),
 ]
--- apps/UserPortal/views.py
+++ apps/UserPortal/views.py
@@ -7,3 +7,6 @@ def index(request):
 
 def about(request):
     return render(request, "UsersideTemplate/about.html")
+
+def ServicesPortal(request):
+    return render(request, "UsersideTemplate/service_portal.html")
