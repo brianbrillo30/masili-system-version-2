b337f60ce1ded85fd4c506bb936b699cc81c287f
Brian Brillo <brillo_3181047@letran-calamba.edu.ph>
2022-09-28 17:26:00 +0800

    Initial Commit

--- apps/BuildingPermit/urls.py
+++ apps/BuildingPermit/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('building_permit_request/',building_permit_module,name='building_permit_module'),
     path('building_permit_list/', building_permit_list, name='building_permit_list'),
     path('building_permit_list/<int:id>', edit_building_permit, name='edit_building_permit'),
+    path('generate_building_permit/<int:id>', generate_building_permit, name='generate_building_permit'),
 ]
--- apps/BuildingPermit/views.py
+++ apps/BuildingPermit/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from apps.UserPortal.models import BuildingPermit
 from .forms import *
+from project.utils import render_to_pdf
 # Create your views here.
 
 def building_permit_module(request):
@@ -22,5 +23,18 @@ def edit_building_permit(request, id):
 
     context = {'form':form, 'disabledform':building_permit}
     return render(request, 'BuildingPermit/building_permit_form.html', context)
+
+
+def generate_building_permit(request, id):
+
+    template_name = "BuildingPermit/building_permit_pdf.html"
+    building_permit = BuildingPermit.objects.get(pk=id)
+    
+    return render_to_pdf(
+        template_name,
+        {
+            "building_permit": building_permit,
+        },
+    )   
 
     
