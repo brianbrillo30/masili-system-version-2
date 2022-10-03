a34c427dc42de9ef2103759579383761c6a53feb
petermamaril <mamaril_3180850@letran-calamba.edu.ph>
2022-10-04 00:32:02 +0800

    initial

--- apps/AnnouncementManagement/views.py
+++ apps/AnnouncementManagement/views.py
@@ -1,3 +1,5 @@
+from fileinput import filename
+import os
 from django.shortcuts import render,redirect, HttpResponse
 
 from apps.AnnouncementManagement.forms import AnnouncementForm
@@ -29,3 +31,47 @@ def add_announcement(request):
             form.save()
             return HttpResponse(status=204, headers={'HX-Trigger': 'announcementAdd'})
     return render (request, 'AnnouncementPage/announcement_form.html', {'form': form})
+
+
+def edit_announcement(request, id):
+    edit = Announcement.objects.get(id=id)
+
+    if request.method == "POST":
+        if len(request.FILES) != 0:
+            if len(edit.image.path) > 0:
+                os.remove(edit.image.path)
+            edit.image = request.FILES['image']
+        edit.title = request.POST.get('title')
+        edit.body = request.POST.get('body')
+        edit.save()
+        return redirect('announcementPage')
+            
+    context = {'edit': edit}
+    return render(request, 'AnnouncementPage/edit_announcement.html',context)
+    # form = AnnouncementForm(instance=edit)
+
+    # if request.method == 'POST':
+    #     form = AnnouncementForm(request.POST,instance=edit)
+    #     img = request.POST.get('image')
+    #     if form.is_valid():
+    #          if img == None:
+    #             annupdate = form.save(commit=False)
+    #             annupdate.image.name = filename+".jpg"
+    #             annupdate.save()
+
+    #             form.save()
+    #             return redirect('announcementPage')
+    # context = {'form':form, "editA": editA, 'prev_img':editA.image}
+    
+
+
+def delete_announcement(request, id):
+    ann = Announcement.objects.get(id=id)
+
+    context = {'ann': ann}
+    if request.method == 'POST':
+        if len(ann.image) > 0:
+            os.remove(ann.image.path)
+            ann.delete()
+            return redirect('announcementPage')
+    return render(request, 'AnnouncementPage/delete_announcement.html', context)
