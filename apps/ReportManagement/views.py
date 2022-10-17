353b9c423cdfce430d612dd9d6d85eaece0bbdc1
Brian Brillo <brillo_3181047@letran-calamba.edu.ph>
2022-10-17 21:26:03 +0800

    Initial Commit

--- apps/ReportManagement/views.py
+++ apps/ReportManagement/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 @cache_control(no_cache=True,must_revalidate=True,no_store=True)
 @login_required(login_url="loginPage")
 @admin_only
+
 def reports(request):
     if request.user.is_authenticated:
         clearance_list = clearance.objects.filter(status=3)
@@ -28,4 +29,56 @@ def reports(request):
         return redirect('loginPage')
 
 
+def clearance_reports(request):
+    if request.user.is_authenticated:
+        clearance_list = clearance.objects.filter(status=3)
+        context = {
+            'clearance_list': clearance_list
+        }
+        return render (request, 'ReportManagement/clearance_reports.html', context)
+    else:
+        return redirect('loginPage')
+
+
+def indigency_reports(request):
+    if request.user.is_authenticated:
+        indigency_list = CertificateOfIndigency.objects.filter(status=3)
+        context = {
+            'indigency_list': indigency_list
+        }
+        return render (request, 'ReportManagement/indigency_reports.html', context)
+    else:
+        return redirect('loginPage')
+
+def business_reports(request):
+    if request.user.is_authenticated:
+        business_list = BusinessPermit.objects.filter(status=3)
+        context = {
+            'business_list' : business_list
+        }
+        return render (request, 'ReportManagement/business_reports.html', context)
+    else:
+        return redirect('loginPage')
+
+
+def building_reports(request):
+    if request.user.is_authenticated:
+        building_list = BuildingPermit.objects.filter(status=3)
+        context = {
+            'building_list' : building_list,
+        }
+        return render (request, 'ReportManagement/building_reports.html', context)
+    else:
+        return redirect('loginPage')
+
+
+def residency_reports(request):
+    if request.user.is_authenticated:
+        residency_list = ResidencyCertificate.objects.filter(status=3)
+        context = {
+            'residency_list': residency_list
+        }
+        return render (request, 'ReportManagement/residency_reports.html', context)
+    else:
+        return redirect('loginPage')
 
