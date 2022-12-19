ca3fabd7630f65affa7c6f00d6bc69154413b802
Brian Brillo <brillo_3181047@letran-calamba.edu.ph>
2022-12-19 15:58:38 +0800

    Initial Commit

---
+++ apps/RequestLogs/decorators.py
@@ -0,0 +1,22 @@
+from django.shortcuts import redirect
+
+
+def admin_only(view_func):
+    def wrapper_function(request, *args, **kwargs):
+        group = None
+        if request.user.groups.exists():
+            
+            group = request.user.groups.all()[0].name
+            
+        if group == 'resident':
+        
+            return redirect('service_portal')
+
+        if group == 'superadmin':
+        
+            return view_func(request, *args, **kwargs)
+
+        if group == 'admin':
+            return view_func(request, *args, **kwargs)
+
+    return wrapper_function
--- apps/RequestLogs/views.py
+++ apps/RequestLogs/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from apps.UserPortal.models import *
+from .decorators import admin_only
+from django.views.decorators.cache import cache_control
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@cache_control(no_cache=True,must_revalidate=True,no_store=True)
+@login_required(login_url="loginPage")
+@admin_only
 
 def requestLogs(request):
-    return render (request, 'RequestLogs/requested_document_logs.html' )
+    if request.user.is_authenticated:
+        clearance_list = clearance.objects.all()
+        indigency_list = CertificateOfIndigency.objects.all()
+        business_list = BusinessPermit.objects.all()
+        building_list = BuildingPermit.objects.all()
+        residency_list = ResidencyCertificate.objects.all()
+        context = {
+            'clearance_list': clearance_list, 
+            'indigency_list': indigency_list,
+            'business_list' : business_list,
+            'building_list' : building_list,
+            'residency_list': residency_list
+        }
+        return render (request, 'RequestLogs/requested_document_logs.html', context )
+    else:
+        return redirect('loginPage')
